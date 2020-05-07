a1f401332e0201dae9157e90dc33d498604ddd27
Weerawat Beer <weerawat.ponprathom@gmail.com>
2020-05-07 13:44:47 +0700

    Update MenuUser.js

--- src/components/files/MenuUser.js
+++ src/components/files/MenuUser.js
@@ -1,47 +1,106 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useStyles from './StyleFiles';
 import { useDispatch } from 'react-redux';
 import { Switch } from '@material-ui/core/';
 import { updateActiveUser } from '../../actions/authActions';
 const MenuUser = (props) => {
-  const { userData } = props;
+  const { userData, refresh } = props;
   const dispatch = useDispatch();
   const classes = useStyles();
 
-  const [check, setCheck] = useState({
-    checked: userData.user_active,
+  const [switchstatus, setSwitchstatus] = useState({
+    switchchecked: userData.user_active,
+  });
+  // useEffect(() => {
+  //   setUser({
+  //     user_id: userData.user_id,
+  //     user_active: '',
+  //     authorized_id: userData.authorized_id,
+  //   });
+  // }, []);
+
+  const [user, setUser] = useState({
+    user_id: userData.user_id,
+    user_active: userData.user_active,
+    authorized_id: userData.authorized_id,
   });
 
-  const handleChange = () => {
+  const { switchchecked } = switchstatus;
+
+  console.log(user);
+
+  // const handleChangeInput = () => {
+  //   setSwitchstatus({ ...switchstatus, switchchecked: !switchchecked });
+  //   if (switchchecked === true) {
+  //     setUser({ ...user, user_active: true });
+  //     dispatch(updateActiveUser(user));
+  //     console.log(user);
+  //   }
+  //   if (switchchecked === false) {
+  //     setUser({ ...user, user_active: false });
+  //     dispatch(updateActiveUser(user));
+  //     console.log(user);
+  //   }
+  // };
+
+  // const handleChange = (event) => {
+  //   setSwitchstatus({ ...switchstatus, switchchecked: !switchchecked });
+  //   setUser({ ...user, user_active: event.target.value });
+  //   // console.log('Checked >>> ' + switchchecked);
+  //   // console.log('Value >>> ' + event.target.value);
+
+  //   // if (userData.user_active === true) {
+  //   //   await setUser({ ...user, user_active: false });
+  //   //   await dispatch(updateActiveUser(user));
+  //   //   console.log('State Check ' + switchchecked);
+  //   //   console.log('from value ' + switchchecked);
+  //   // }
+  //   // if (userData.user_active === false) {
+  //   //   await setUser({ ...user, user_active: true });
+  //   //   await dispatch(updateActiveUser(user));
+  //   //   console.log('State Check ' + switchchecked);
+  //   //   console.log('from value ' + switchchecked);
+  //   // }
+  // };
+
+  const handleChange = async (e) => {
     if (userData.user_active === true) {
-      dispatch(
+      await dispatch(
         updateActiveUser({
           user_id: userData.user_id,
           user_active: false,
           authorized_id: userData.authorized_id,
         })
       );
-      setCheck({ checked: !check.checked });
-    }
-    if (userData.user_active === false) {
-      dispatch(
+
+      console.log('State Check ' + switchchecked);
+      console.log('from value ' + switchchecked);
+    } else if (userData.user_active === false) {
+      await dispatch(
         updateActiveUser({
           user_id: userData.user_id,
           user_active: true,
           authorized_id: userData.authorized_id,
         })
       );
-      setCheck({ checked: !check.checked });
+      console.log('State Check ' + switchchecked);
+      console.log('from value ' + switchchecked);
     }
+    await setSwitchstatus({ ...switchstatus, switchchecked: !switchchecked });
   };
 
   return (
     <div>
       <Switch
-        checked={check.checked}
+        checked={switchchecked}
         onChange={handleChange}
         className={classes.tableMargin}
       ></Switch>
+      {/* <input
+        type='text'
+        value={user.user_active}
+        onChange={handleChangeInput}
+      ></input> */}
     </div>
   );
 };

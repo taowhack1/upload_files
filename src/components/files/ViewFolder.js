b23909aea61245b4b6e53cb6ec1bd012df90be47
taowhack1 <tulagarn007@gmail.com>
2020-05-12 14:15:28 +0700

    Update ViewFolder.js

--- src/components/files/ViewFolder.js
+++ src/components/files/ViewFolder.js
@@ -1,7 +1,7 @@
-import React, { useEffect, Fragment } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, Redirect, useHistory } from 'react-router-dom';
-import moment from 'moment';
+import React, { useEffect, Fragment } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, Redirect, useHistory } from "react-router-dom";
+import moment from "moment";
 import {
   Table,
   TableBody,
@@ -23,113 +23,130 @@ import dateTH from "./function";
 const ViewFolder = () => {
   const classes = useStyles();
   const { folders, loading } = useSelector((state) => state.folder);
-  const { authdata } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getFolders(authdata.user_id));
   }, []);
   const { authenticated, authdata } = useSelector((state) => state.auth);
   if (loading) {
-    console.log('loading >>> ' + loading);
+    console.log("loading >>> " + loading);
   }
 
   return (
     <div>
-      {authenticated &&
+      {authenticated && (
         <div>
-          {authdata.authorized_id == 1
-            ?
-            <Fragment>
-              <Grid container container className={classes.gridContainer}>
-                <Paper className={classes.paper}>
-                  <Grid container>
-                    <Breadcrumbs
-                      className={classes.breadcrumbs}
-                      separator={<NavigateNextIcon />}
-                      aria-label='breadcrumb'
-                    >
-                      <Typography className={classes.text} color='textPrimary'>
-                        โฟลเดอร์ทั้งหมด
-              </Typography>
-                    </Breadcrumbs>
-                  </Grid>
-                </Paper>
-                <Paper className={classes.paper}>
-                  <Table className={classes.table}>
-                    <TableHead>
-                      <TableCell className={classes.tableCellName}>
-                        <Typography color='textPrimary' className={classes.text}>
-                          ชื่อ
-                </Typography>
-                      </TableCell>
-                      <TableCell align='center'>
-                        <Typography color='textPrimary' className={classes.text}>
-                          วันที่แก้ไข
-                </Typography>
-                      </TableCell>
-                      <TableCell align='center'>
+          {
+            authdata.authorized_id == 1 ? (
+              <Fragment>
+                <Grid container container className={classes.gridContainer}>
+                  <Paper className={classes.paper}>
+                    <Grid container>
+                      <Breadcrumbs
+                        className={classes.breadcrumbs}
+                        separator={<NavigateNextIcon />}
+                        aria-label="breadcrumb"
+                      >
                         <Typography
-                          color='textPrimary'
                           className={classes.text}
-                        ></Typography>
-                      </TableCell>
-                    </TableHead>
-                    <TableBody>
-                      {!loading && folders !== null
-                        ? folders.map((row) => (
-                          <TableRow key={row.folder_id}>
-                            <TableCell>
-                              <Link
-                                to={{
-                                  pathname: '/ViewFiles/' + folder.folder_id,
-                                  state: {
-                                    folder_id: row.folder_id,
-                                    folder_name: row.folder_name,
-                                  },
-                                }}
-                              >
-                                <Grid container className={classes.iconAlign}>
-                                  <Grid item></Grid>
-                                  <Grid item xs={1}>
-                                    <FolderIcon className={classes.iconFolderTable} />
-                                  </Grid>
-                                  <Grid item xs={10}>
-                                    <Typography className={classes.text}>
-                                      {row.folder_name}
-                                    </Typography>
-                                  </Grid>
-                                </Grid>
-                              </Link>
-                            </TableCell>
-                            <TableCell align='center'>
-                              <Typography
-                                color='textPrimary'
-                                className={classes.text}
-                              >
-                                {moment
-                                  .utc(folder.folder_updated)
-                                  .add(3, "minutes")
-                                  .format("DD-MM-YYYY HH:mm")}
-                              </Typography>
-                            </TableCell>
-                            <TableCell align='center'></TableCell>
-                          </TableRow>
-                        ))
-                        : console.log('Nodata')}
-                    </TableBody>
-                  </Table>
-                  {loading && (
-                    <div className={classes.loading}>
-                      <Circular />
-                    </div>
-                  )}
-                </Paper>
-              </Grid>
-            </Fragment>
-            : <Redirect to={'/notfound'} />
+                          color="textPrimary"
+                        >
+                          โฟลเดอร์ทั้งหมด
+                        </Typography>
+                      </Breadcrumbs>
+                    </Grid>
+                  </Paper>
+                  <Paper className={classes.paper}>
+                    <Table className={classes.table}>
+                      <TableHead>
+                        <TableCell className={classes.tableCellName}>
+                          <Typography
+                            color="textPrimary"
+                            className={classes.text}
+                          >
+                            ชื่อ
+                          </Typography>
+                        </TableCell>
+                        <TableCell align="center">
+                          <Typography
+                            color="textPrimary"
+                            className={classes.text}
+                          >
+                            วันที่แก้ไข
+                          </Typography>
+                        </TableCell>
+                        <TableCell align="center">
+                          <Typography
+                            color="textPrimary"
+                            className={classes.text}
+                          ></Typography>
+                        </TableCell>
+                      </TableHead>
+                      <TableBody>
+                        {!loading && folders !== null
+                          ? folders.map((row) => (
+                              <TableRow key={row.folder_id}>
+                                <TableCell>
+                                  <Link
+                                    to={{
+                                      pathname:
+                                        "/ViewFiles/" + folder.folder_id,
+                                      state: {
+                                        folder_id: row.folder_id,
+                                        folder_name: row.folder_name,
+                                      },
+                                    }}
+                                  >
+                                    <Grid
+                                      container
+                                      className={classes.iconAlign}
+                                    >
+                                      <Grid item></Grid>
+                                      <Grid item xs={1}>
+                                        <FolderIcon
+                                          className={classes.iconFolderTable}
+                                        />
+                                      </Grid>
+                                      <Grid item xs={10}>
+                                        <Typography className={classes.text}>
+                                          {row.folder_name}
+                                        </Typography>
+                                      </Grid>
+                                    </Grid>
+                                  </Link>
+                                </TableCell>
+                                <TableCell align="center">
+                                  <Typography
+                                    color="textPrimary"
+                                    className={classes.text}
+                                  >
+                                    {moment
+                                      .utc(folder.folder_updated)
+                                      .add(3, "minutes")
+                                      .format("DD-MM-YYYY HH:mm")}
+                                  </Typography>
+                                </TableCell>
+                                <TableCell align="center"></TableCell>
+                              </TableRow>
+                            ))
+                          : console.log("Nodata")}
+                      </TableBody>
+                    </Table>
+                    {loading && (
+                      <div className={classes.loading}>
+                        <Circular />
+                      </div>
+                    )}
+                  </Paper>
+                </Grid>
+              </Fragment>
+            ) : (
+              <Redirect to={"/notfound"} />
+            )
             //history.push('/viewfolderamin')
-          }</div>
-      }
+          }
+        </div>
+      )}
     </div>
   );
 };

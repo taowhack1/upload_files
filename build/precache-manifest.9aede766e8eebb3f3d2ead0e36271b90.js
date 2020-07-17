b2d4ea5aef92005f1870923f0e0259348cf88cc5
Weerawat Beer <weerawat.ponprathom@gmail.com>
2020-07-17 14:41:46 +0700

    Update Package.json

---
+++ build/precache-manifest.9aede766e8eebb3f3d2ead0e36271b90.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e7a5fe6d8ac16ae4f3292fd30d2c94",
+    "url": "/upload_files/index.html"
+  },
+  {
+    "revision": "1addd66b6a0a07d947d9",
+    "url": "/upload_files/static/css/main.28704c7f.chunk.css"
+  },
+  {
+    "revision": "e50d16c4956b8e8296e2",
+    "url": "/upload_files/static/js/2.077055fc.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/upload_files/static/js/2.077055fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1addd66b6a0a07d947d9",
+    "url": "/upload_files/static/js/main.5429d4fb.chunk.js"
+  },
+  {
+    "revision": "89a81d41106d7583de95",
+    "url": "/upload_files/static/js/runtime-main.18e44462.js"
+  },
+  {
+    "revision": "c72dfa7018d295305acc5ec4666fc940",
+    "url": "/upload_files/static/media/document.c72dfa70.svg"
+  },
+  {
+    "revision": "5ffbc9bc9f69b085db3617e229ce4479",
+    "url": "/upload_files/static/media/jpg.5ffbc9bc.svg"
+  },
+  {
+    "revision": "8528c927572ec5f3768bc2d18e395a12",
+    "url": "/upload_files/static/media/logo.8528c927.svg"
+  },
+  {
+    "revision": "e9b3fa5ed9e6557a7c0f1ff7674a6781",
+    "url": "/upload_files/static/media/options.e9b3fa5e.svg"
+  },
+  {
+    "revision": "3fa92281020f448a50d540d268e7fa3e",
+    "url": "/upload_files/static/media/pdf.3fa92281.svg"
+  },
+  {
+    "revision": "832ce32cc4144b1d5886292168de4964",
+    "url": "/upload_files/static/media/png.832ce32c.svg"
+  },
+  {
+    "revision": "6ef89bbbb8afea2e14fe40ee349daf7d",
+    "url": "/upload_files/static/media/powerpoint.6ef89bbb.svg"
+  },
+  {
+    "revision": "8983289887bd4940e52da65b0aa62b5c",
+    "url": "/upload_files/static/media/txt.89832898.svg"
+  },
+  {
+    "revision": "15eeae14a20ce229ca17f47f16386077",
+    "url": "/upload_files/static/media/user.15eeae14.svg"
+  },
+  {
+    "revision": "152573d4d3577db66f27ac022eba69a5",
+    "url": "/upload_files/static/media/xls.152573d4.svg"
+  },
+  {
+    "revision": "1ead5aed0c0cd24785b4e2bdd92274a2",
+    "url": "/upload_files/static/media/zip.1ead5aed.svg"
+  }
+]);
